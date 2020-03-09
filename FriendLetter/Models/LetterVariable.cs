19527ecd0c95d5d9c6bef522681d2c57095f0901
K. Wicz and Rachel Schieferstein <k.@>
2020-03-09 10:38:48 -0700

    adding files with form functionality

---
+++ FriendLetter/Models/LetterVariable.cs
@@ -0,0 +1,10 @@
+namespace FriendLetter.Models
+{
+  public class LetterVariable
+  {
+    public string Recipient { get; set; }
+    public string Sender { get; set; }
+    public string Locations { get; set; }
+    public string Souvenirs { get; set; }
+  }
+}
